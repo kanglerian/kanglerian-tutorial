130c2ba12fe124298de5b78651c5ec21e9c93040
kanglerian <kanglerian@gmail.com>
2023-03-06 20:05:37 +0700

    add number

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -4,7 +4,11 @@ import Dummy from '../images/dummy.jpg'
 import { Link } from 'react-router-dom';
 
 const Course = () => {
-  const [video, setVideo] = useState('12TuC1sUXxM');
+  const [video, setVideo] = useState({
+    id: '',
+    name: 'Tutorial Online Kini Hadir!',
+    video: '12TuC1sUXxM'
+  });
   // eslint-disable-next-line
   const [courses, setCourses] = useState([
     {
@@ -66,7 +70,7 @@ const Course = () => {
               {
                 courses.map((course, i) => {
                   return (
-                    <li key={i} onClick={() => setVideo(course.video)}>
+                    <li key={i} onClick={() => setVideo({ id: i + 1, name: course.name, video: course.video })}>
                       <div className='flex items-center p-2 transition ease-in-out border border-transparent duration-200 hover:border-slate-200 hover:translate-x-2 rounded-xl gap-5' role='button'>
                         <img src={Dummy} alt='' style={{ width: 55, height: 55 }} className='rounded-xl' />
                         <div className='space-y-1'>
@@ -81,9 +85,9 @@ const Course = () => {
             </ul>
           </div>
           <div className='order-1 md:order-none basis-3/6 rounded-3xl space-y-5 h-screen overflow-hidden'>
-            <iframe className='w-full h-52 md:h-80 rounded-3xl' src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe className='w-full h-52 md:h-80 rounded-3xl' src={`https://www.youtube.com/embed/${video.video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <div className='space-y-4'>
-              <h1 className='text-gray-900 font-bold text-2xl'>Graphic Design Spesialization</h1>
+              <h1 className='text-gray-900 font-bold text-2xl'>{`${video.id}${video.id !== '' ? '.' : ''} ${video.name}`}</h1>
               <p className='text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu libero facilisis, commodo elit sed, dapibus dolor. Donec non efficitur dui, sit amet vestibulum est. Curabitur massa augue, luctus vitae finibus id, tempor at lacus.</p>
               <div className='flex items-center gap-2'>
                 <img src={Dummy} alt='' className="rounded-full" style={{ width: 40, height: 40 }} />
