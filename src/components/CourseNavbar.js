4da928faf4151075375ae2a8cc9db320fc918335
kampusinformatika <kanglerian.business@gmail.com>
2023-03-07 18:10:56 +0700

    dark mode

--- src/components/CourseNavbar.js
+++ src/components/CourseNavbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Google from '../images/googleplay.webp'
-import Dummy from '../images/dummy.jpg'
+import Photo from '../images/lerian.jpg'
 import Logo from '../images/react.svg'
 import { Link, NavLink, useLocation } from 'react-router-dom';
 
@@ -9,19 +9,18 @@ const CourseNavbar = () => {
   const location = useLocation();
   const { pathname } = location;
   return (
-    <div className='text-gray-800 text-sm'>
+    <div className='text-gray-800 dark:text-slate-100 text-sm'>
       <nav className='container mx-auto px-4'>
         <div className='flex flex-wrap flex-col md:flex-row justify-start md:items-center md:justify-between md:gap-10 py-4'>
           <div className='flex flex-wrap items-center justify-between gap-3 md:gap-10'>
-            <Link to='/' className='flex items-center gap-3'><img src={Logo} alt='logo' className='h-8 transition ease-in-out duration-500 hover:rotate-180' /><span className='font-bold text-lg'>Tutorial Online</span></Link>
+            <Link to='/' className='flex items-center gap-3'><img src={Logo} alt='logo' className='h-8 transition ease-in-out duration-500 hover:rotate-180' /></Link>
             <form className='inline'>
               <div>
-                <span><i class="fa-solid fa-magnifying-glass"></i></span>
-                <input placeholder='What do you want to learn?' className='text-gray-600 text-xs px-3 py-1 w-52 focus:outline-none'/>
+                <span className='mr-2'><i class="fa-solid fa-magnifying-glass"></i></span>
+                <input placeholder='What do you want to learn?' className='dark:bg-slate-800 text-slate-600 dark:text-slate-400 text-xs px-3 py-1 w-52 focus:outline-none'/>
               </div>
             </form>
-            <button className='hover:bg-slate-100 py-2 px-3 rounded'><i class="fa-solid fa-moon"></i></button>
-            <button className='md:hidden hover:bg-slate-100 transition ease-in-out duration-500 hover:rotate-180 py-2 px-3 rounded' onClick={() => setVisible(!visible)}><i class="fa-solid fa-bars"></i></button>
+            <button className='md:hidden hover:bg-slate-100 hover:dark:bg-slate-700 transition ease-in-out duration-500 hover:rotate-180 py-2 px-3 rounded' onClick={() => setVisible(!visible)}><i class="fa-solid fa-bars"></i></button>
           </div>
           <div className={`${visible === true ? '' : 'hidden'} md:block w-full md:w-auto md:mt-0`}>
             <ul className='flex flex-col justify-center mt-5 md:mt-0 md:flex-row md:space-x-2 md:space-y-0 space-y-3'>
@@ -32,8 +31,8 @@ const CourseNavbar = () => {
                 <li className='hover:bg-slate-100 md:hover:bg-transparent px-4 py-1 rounded' role='button'>Purchases</li>
               </NavLink>
               <NavLink className={pathname === "/account" ? "font-bold" : ""} to='/courses'>
-                <li className='flex items-center justify-between gap-2 border border-gray-100 px-1 text-xs py-1 rounded-full' role='button'>
-                  <img src={Dummy} alt='' className='rounded-full' style={{ width:25, height:25 }}/>
+                <li className='flex items-center justify-between gap-2 border border-slate-200 dark:border-slate-600 px-1 text-xs py-1 rounded-full' role='button'>
+                  <img src={Photo} alt='' className='rounded-full' style={{ width:25, height:25 }}/>
                   <span>Lerian Febriana</span>
                   <span className='pr-2'><i class="fa-solid fa-right-from-bracket"></i></span>
                 </li>
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,28 +1,32 @@
 import React, { useState } from 'react'
 import CourseNavbar from '../components/CourseNavbar'
-import Dummy from '../images/dummy.jpg'
+import Photo from '../images/lerian.jpg'
 import { Link } from 'react-router-dom';
 
 const Course = () => {
   const [video, setVideo] = useState({
     id: '',
     name: 'Tutorial Online Kini Hadir!',
-    video: '12TuC1sUXxM'
+    video: '12TuC1sUXxM',
+    desc: 'Lorem Ipsum dolor sit amet.'
   });
   // eslint-disable-next-line
   const [courses, setCourses] = useState([
     {
       id: 1,
       name: 'Penjelasan ROAD MAP',
-      video: 'NRQKZA-JkSI'
+      video: 'NRQKZA-JkSI',
+      desc: 'Lorem Ipsum dolor sit amet.'
     }, {
       id: 2,
       name: 'Penjelasan JSON Server',
-      video: 'rPEOw9_nrjk'
+      video: 'rPEOw9_nrjk',
+      desc: 'Lorem Ipsum dolor sit amet.'
     }, {
       id: 3,
       name: 'Penjelasan Figma Dasar',
-      video: 'fqyoMgVuu5s'
+      video: 'fqyoMgVuu5s',
+      desc: 'Lorem Ipsum dolor sit amet.'
     }
   ]);
   // eslint-disable-next-line
@@ -50,32 +54,32 @@ const Course = () => {
     },
   ]);
   return (
-    <div className='md:h-screen overflow-hidden'>
+    <div className='md:h-screen overflow-hidden dark:bg-slate-800'>
       <CourseNavbar />
       <div className='container mx-auto px-4 py-5'>
         <div className='flex flex-col md:flex-row justify-between gap-5'>
-          <div className='order-3 md:order-none basis-1/6 text-gray-900 h-screen overflow-y-auto'>
+          <div className='order-3 md:order-none basis-1/6 text-slate-900 dark:text-slate-100 h-screen overflow-y-auto'>
             <h1 className='text-3xl font-bold'>
               <Link className='flex items-center gap-3'><span className='font-bold text-2xl'>Explore</span></Link>
             </h1>
-            <ul className='mt-5 space-y-5 text-sm text-gray-600'>
-              <li className='text-sky-600 font-medium'>Front End Development</li>
-              <li>Back End Development</li>
-              <li>Desain Grafis</li>
-              <li>Animasi</li>
+            <ul className='mt-5 space-y-5 text-sm text-slate-700 dark:text-slate-200'>
+              <li className='text-sky-500 font-medium' role='button'>Front End Development</li>
+              <li role='button'>Back End Development</li>
+              <li role='button'>Desain Grafis</li>
+              <li role='button'>Animasi</li>
             </ul>
           </div>
-          <div className='order-2 md:order-none basis-2/6 h-screen bg-slate-50 rounded-3xl py-6 px-6 overflow-y-auto'>
+          <div className='order-2 md:order-none basis-2/6 h-screen bg-slate-50 dark:bg-slate-700 rounded-3xl py-6 px-6 overflow-y-auto'>
             <ul className='space-y-1'>
               {
                 courses.map((course, i) => {
                   return (
-                    <li key={i} onClick={() => setVideo({ id: i + 1, name: course.name, video: course.video })}>
-                      <div className='flex items-center p-2 transition ease-in-out border border-transparent duration-200 hover:border-slate-200 hover:translate-x-2 rounded-xl gap-5' role='button'>
-                        <img src={Dummy} alt='' style={{ width: 55, height: 55 }} className='rounded-xl' />
+                    <li key={i} onClick={() => setVideo({ id: i + 1, name: course.name, video: course.video, desc: course.desc })}>
+                      <div className='flex items-center p-2 transition ease-in-out border border-transparent duration-200 hover:border-slate-200 hover:dark:border-slate-600 hover:translate-x-2 rounded-xl gap-5' role='button'>
+                        <span className='flex items-center justify-center bg-orange-400 w-24 h-14 rounded-xl text-white'><i class="transition ease-in-out duration-700 hover:rotate-180 fa-solid fa-ellipsis fa-2x"></i></span>
                         <div className='space-y-1'>
-                          <h3 className='text-gray-900 font-medium text-sm'>{course.name}</h3>
-                          <p className='text-xs text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                          <h3 className='text-slate-800 dark:text-slate-100 font-medium text-sm'>{course.name}</h3>
+                          <p className='text-xs text-slate-600 dark:text-slate-400'>{course.desc}</p>
                         </div>
                       </div>
                     </li>
@@ -84,19 +88,18 @@ const Course = () => {
               }
             </ul>
           </div>
-          <div className='order-1 md:order-none basis-3/6 rounded-3xl space-y-5 h-screen overflow-hidden'>
-            <iframe className='w-full h-52 md:h-80 rounded-3xl' src={`https://www.youtube.com/embed/${video.video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div className='order-1 md:order-none basis-3/6 space-y-5 h-screen overflow-hidden'>
+            <iframe className='w-full h-56 md:h-80 rounded-2xl md:rounded-3xl' src={`https://www.youtube.com/embed/${video.video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <div className='space-y-4'>
-              <h1 className='text-gray-900 font-bold text-2xl'>{`${video.id}${video.id !== '' ? '.' : ''} ${video.name}`}</h1>
-              <p className='text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu libero facilisis, commodo elit sed, dapibus dolor. Donec non efficitur dui, sit amet vestibulum est. Curabitur massa augue, luctus vitae finibus id, tempor at lacus.</p>
-              <div className='flex items-center gap-2'>
-                <img src={Dummy} alt='' className="rounded-full" style={{ width: 40, height: 40 }} />
+              <h1 className='text-slate-800 dark:text-slate-100 font-bold text-2xl'>{`${video.id}${video.id !== '' ? '.' : ''} ${video.name}`}</h1>
+              <p className='text-slate-600 dark:text-slate-400 text-sm'>{video.desc}</p>
+              <div className='flex items-center gap-2 border-t dark:border-slate-600 pt-5'>
+                <img src={Photo} alt='' className="rounded-full" style={{ width: 40, height: 40 }} />
                 <div>
-                  <h4 className='text-sm text-gray-900 font-medium'>Lerian Febriana, A.Md.Kom</h4>
-                  <p className='text-xs text-gray-500'>Software Engineer</p>
+                  <h4 className='text-sm text-slate-800 dark:text-slate-100 font-medium'>Lerian Febriana, A.Md.Kom</h4>
+                  <p className='text-xs text-slate-600 dark:text-slate-400'>Software Engineer</p>
                 </div>
               </div>
-              <hr />
               {/* <div className='flex flex-wrap'>
                 {
                   benefits.map((benefit, i) => {
